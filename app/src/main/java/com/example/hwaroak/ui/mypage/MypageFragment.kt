b9bc00dbff59afd4b001973d1890f61c0594e354
장기준 <autojjangs@naver.com>
2025-07-12 23:30:17 +0900

    #24 [스탠다드/UI] - 마이페이지 기본 화면 MaterialButton 속성 수정

--- app/src/main/java/com/example/hwaroak/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/hwaroak/ui/mypage/MypageFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat.animate
 import com.example.hwaroak.R
 import com.example.hwaroak.databinding.FragmentMypageBinding
@@ -41,11 +42,12 @@ class MypageFragment : Fragment() {
         )
 
         val pieColors = listOf(
-            resources.getColor(R.color.comfy, null),
-            resources.getColor(R.color.happy, null),
-            resources.getColor(R.color.depressed, null),
-            resources.getColor(R.color.angry, null)
+            ContextCompat.getColor(requireContext(), R.color.comfy),
+            ContextCompat.getColor(requireContext(), R.color.happy),
+            ContextCompat.getColor(requireContext(), R.color.depressed),
+            ContextCompat.getColor(requireContext(), R.color.angry)
         )
+
         val dataSet = PieDataSet(emotionRatio, "")
 
         dataSet.colors = pieColors
